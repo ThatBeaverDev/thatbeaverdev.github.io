dd71aaa9937fc325549d1a5c53346c3850ad31fd
TNKS-Development <107750535+TeknikSpace@users.noreply.github.com>
2023-09-11 08:09:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "5vw";
 document.getElementsByTagName("h2")[0].style.fontSize = "2vw";
+document.getElementsByTagName("p1")[0].style.fontSize = "1vw";
+document.getElementsByTagName("a1")[0].style.fontSize = "1vw";
+
+
+
+  // Get Favicon
+          let link = document.querySelector("link[rel~='icon']");
+          if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.head.appendChild(link);
+  
+          link.href = "https://avatars.githubusercontent.com/u/107750535?v=4";
+            
+document.title = "Teknikspace.github.io";
