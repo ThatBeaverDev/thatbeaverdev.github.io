245c34984178062e7c51929b05964ffadb57128f
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-01-26 19:25:59 +0000

    Remake Site

---
+++ header.js
@@ -0,0 +1,24 @@
+async function run() {
+    async function getData() {
+        const url = "/header.html";
+        try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const json = await response.text();
+        return json
+        } catch (error) {
+        console.error(error.message);
+        }
+    }
+    
+
+    let element = document.getElementById("mainHeader")
+    let innerHTML = await getData()
+    console.log(innerHTML)
+    element.innerHTML = innerHTML
+}
+
+run()
